3eea08d7e2d2db19d2508a29c6fd12ec6e643c7b
Владимир Ласовский <herrShneider@gmail.com>
2024-05-07 18:00:06 +0200

    configured data import import_csv

--- backend/recipes/management/commands/import_csv.py
+++ backend/recipes/management/commands/import_csv.py
@@ -1,28 +1,46 @@
-"""Manage - команда для импорта csv-файлов."""
-
+import base64
 import csv
 
+from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
+from django.utils.timezone import now
 
-from recipes.models import Ingredient, Tag, Recipe
+from recipes.models import Ingredient, Tag, Recipe, User, IngredientRecipe
 
 csv_files = [
+    'users.csv',
     'ingredients.csv',
     'tags.csv',
-    # 'recipes.csv',
+    'recipes.csv',
 ]
 
 csv_fields = {
+    'users.csv': ['email', 'username', 'first_name', 'last_name', 'password', 'role'],
     'ingredients.csv': ['name', 'measurement_unit'],
     'tags.csv': ['name', 'color', 'slug'],
     'recipes.csv': ['name', 'text', 'cooking_time'],
 }
 
 models = {
+    'users': User,
     'ingredients': Ingredient,
     'tags': Tag,
     'recipes': Recipe,
 }
+
+
+def make_image_file(data):
+    """Разбивает по ;base64, на заголовок и строковый код картинки."""
+    if not data:
+        return None
+    header, imagestr = data.split(';base64,')
+    extension = header.split('/')[-1]
+    str_now_time = str(now().time())
+    filename = f'image_{str_now_time}.{extension}'
+    return ContentFile(
+        base64.b64decode(imagestr),
+        name=filename
+    )
 
 
 def csv_reader_file(csv_file_name):
@@ -37,7 +55,8 @@ def csv_reader_file(csv_file_name):
 
 
 class Command(BaseCommand):
-    """Класс команды."""
+
+    image_string = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAgMAAABieywaAAAACVBMVEUAAAD///9fX1/S0ecCAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAACklEQVQImWNoAAAAggCByxOyYQAAAABJRU5ErkJggg==',
 
     def handle(self, *args, **options):
         """Функция валидации полей и импорта."""
@@ -45,24 +64,56 @@ class Command(BaseCommand):
             model = csv_file_name.split('.')[0]
             model_class = models.get(model)
             for row in csv_reader_file(csv_file_name):
-                item = None
-                if model_class == Ingredient:
-                    item = Ingredient(
+                if model_class == Recipe:
+                    tags_queryset = Tag.objects.filter(id__in=row['tags'].split('%'))
+                    ingredients_queryset = Ingredient.objects.filter(id__in=row['ingredients'].split('%'))
+                    author = User.objects.get(username=row['author'])
+                    recipe = Recipe.objects.create(
+                        author=author,
                         name=row['name'],
-                        measurement_unit=row['measurement_unit'],
-                    )
-                elif model_class == Tag:
-                    item = Tag(
-                        name=row['name'],
-                        color=row['color'],
-                        slug=row['slug'],
-                    )
-                elif model_class == Recipe:
-                    item = Recipe(
-                        name=row['name'],
+                        image=make_image_file(self.image_string[0]),
                         text=row['text'],
                         cooking_time=row['cooking_time'],
                     )
+                    for ingredient_pk in row['ingredients'].split('%'):
+                        ingredient_recipe = IngredientRecipe.objects.create(
+                            ingredient_id=ingredient_pk,
+                            recipe=recipe,
+                            amount=row['amount']
+                        )
+                    recipe.tags.set(tags_queryset)
+                    recipe.ingredients.set(ingredients_queryset)
+                elif model_class in (User, Ingredient, Tag):
+                    item = None
+                    if model_class == User:
+                        item = User(
+                            email=row['email'],
+                            username=row['username'],
+                            first_name=row['first_name'],
+                            last_name=row['last_name'],
+                            password=row['password'],
+                            role=row['role'],
+                        )
+                    elif model_class == Ingredient:
+                        item = Ingredient(
+                            name=row['name'],
+                            measurement_unit=row['measurement_unit'],
+                        )
+                    elif model_class == Tag:
+                        item = Tag(
+                            name=row['name'],
+                            color=row['color'],
+                            slug=row['slug'],
+                        )
+                    else:
+                        self.stdout.write(
+                            self.style.ERROR(
+                                f'Не удалось создать запись {model}'
+                            )
+                        )
+                        continue
+                    item.full_clean()
+                    item.save()
                 else:
                     self.stdout.write(
                         self.style.ERROR(
@@ -70,8 +121,6 @@ class Command(BaseCommand):
                         )
                     )
                     continue
-                item.full_clean()
-                item.save()
                 self.stdout.write(
                     self.style.SUCCESS(
                         f'Запись для модели {model} добавлена: {row}'
